273dec68142219e359c0f7b73bf56d0301745834
Joel Holdsworth <joel.holdsworth@vcatechnology.com>
2017-05-17 17:57:31 -0600

    timelapse: Make audio optional

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -15,6 +15,8 @@ parser.add_argument('in_files', metavar='IN', type=str, nargs='+',
     help='The output file')
 parser.add_argument('out_file', metavar='OUT', type=str, nargs=1,
     help='The input files', default='out.mp4')
+parser.add_argument('-a', '--with-audio', action='store_true',
+    help='Include audio')
 args = parser.parse_args()
 
 factor = args.factor[0]
@@ -31,13 +33,18 @@ with open(concat_file, 'w') as f:
     for i in in_files:
         f.write("file '" + os.path.abspath(i) + "'\n")
 
+filter_complex = ('[0:v]' + 'tblend=average,framestep=2,' * logf +
+    'setpts={0}*PTS'.format(1.0 / factor) + '[v]')
+if args.with_audio:
+    filter_complex += ';[0:a]' + ','.join(['atempo=2.0'] * logf) + '[a]'
 
-cmd = ['ffmpeg', '-f', 'concat', '-safe', '0', '-i', concat_file, '-y',
-        '-filter_complex',
-        '[0:v]' + 'tblend=average,framestep=2,' * logf +
-            'setpts={0}*PTS'.format(1.0 / factor) + '[v];'
-            '[0:a]' + ','.join(['atempo=2.0'] * logf) + '[a]',
-        '-map', '[v]', '-map', '[a]', '-r', '30', out_file]
+mappings = ['-map', '[v]']
+if args.with_audio:
+    mappings += ['-map', '[a]']
+
+cmd = (['ffmpeg', '-f', 'concat', '-safe', '0', '-i', concat_file, '-y',
+        '-filter_complex', filter_complex] +
+        mappings + ['-r', '30', out_file])
 print(' '.join(cmd))
 call(cmd)
 
